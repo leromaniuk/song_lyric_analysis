5516d34f9fdcc4178bc8f96ff232746ac000f39d
Louise Romaniuk <elleromaniuk@hotmail.co.uk>
2022-09-26 00:22:46 +0100

    feat: added new file to apply basic cleaning to lyrics dataframe

---
+++ modules/data_cleaning.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 21:13:58 2022
+
+@author: eller
+"""
+
+def data_checks(df):
+    '''
+    Function prints simple data checks for collected pd dataframe
+        - counts track duplicates
+        - counts missing traks and lyrics
+    
+    Parameters:
+        df (DataFrame): df containing artist, album/ tracks
+    '''
+    print("data shape: ")
+    print(df.shape)
+    
+    print("missing track values: ")
+    print(df['track'].isna().sum())
+    print("missing lyric values: ")
+    print(df['lyric'].isna().sum())
+    
+    print("duplicated tracks: ")
+    print(df\
+          .duplicated(subset=["album", "artist", "track"], keep='first')\
+          .sum())
+        
+def data_cleaning(data):
+    '''
+    Function provides basic cleaning for collected dataframe 
+        - removes rows with track duplicates
+        - removes rows with traks and lyrics
+    
+    
+    Parameters:
+        df (DataFrame): df containing artist, album/ tracks
+
+    Returns:
+        df (DataFrame): cleaned df
+    '''
+    
+    data = data.drop_duplicates(subset=["album", "artist", "track"], keep='first')
+    
+    data = data.dropna(subset=['track'])
+    data = data.dropna(subset=['lyric'])
+    
+    return data
