9b88d1df523098ac65daaeb9322bed3bfbde9360
Louise Romaniuk <elleromaniuk@hotmail.co.uk>
2022-10-02 13:56:14 +0100

    refactor: deleted preprocess_lyrics.py and moved functions to data_cleaning.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Created on Sun Sep 25 17:33:53 2022
 
 import modules.get_song_lyrics as gl
 import modules.data_cleaning as dc
-import modules.preprocess_lyrics as pl
 
 lyrics = gl.import_data(overwrite=False)
 dc.data_checks(lyrics)
@@ -16,12 +15,19 @@ lyrics_cleaned = dc.data_cleaning(lyrics)
 dc.data_checks(lyrics_cleaned)
 
 lyrics_cleaned['lyric_cleaned'] = lyrics_cleaned.copy()['lyric']
-frequent_lines = pl.get_frequent_lines(lyrics_cleaned['lyric_cleaned'], 
+frequent_lines = dc.get_frequent_lines(lyrics_cleaned['lyric_cleaned'], 
                     word_count = 6, char_count = 20, freq = 10)
-first_lines = pl.get_first_lines(lyrics_cleaned['lyric_cleaned'])
-len(common_lines)
+first_lines = dc.get_first_lines(lyrics_cleaned['lyric_cleaned'])
+len(frequent_lines)
 len(first_lines)
 
 
-lyrics_cleaned['lyric_cleaned'] = pl.clean_lyrics(lyrics_cleaned['lyric_cleaned'],
+lyrics_cleaned = lyrics_cleaned.loc[0:10]
+lyrics_cleaned['lyric_cleaned'] = dc.clean_lyrics(lyrics_cleaned['lyric_cleaned'],
              boiler = frequent_lines + first_lines)
+
+
+
+
+
+
